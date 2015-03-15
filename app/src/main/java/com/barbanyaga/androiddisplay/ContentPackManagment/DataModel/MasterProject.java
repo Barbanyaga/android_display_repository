232c0304feea01a9212e1b1b08b5b1ac790ae52b
barbanyaga <barbanyaga@gmail.com>
2015-03-15 23:05:04 +0400

    Модель данных

---
+++ app/src/main/java/com/barbanyaga/androiddisplay/ContentPackManagment/DataModel/MasterProject.java
@@ -0,0 +1,10 @@
+package com.barbanyaga.androiddisplay.ContentPackManagment.DataModel;
+
+import java.util.ArrayList;
+
+/**
+ * Created by barbanyaga on 15.03.2015.
+ */
+public class MasterProject {
+    ArrayList<Project> Projects;
+}
