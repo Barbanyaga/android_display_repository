3650aba2760a78f1308ed60389ba7628de1d0a95
barbanyaga <barbanyaga@gmail.com>
2015-03-22 14:22:11 +0400

    Импорт XML

---
+++ app/src/main/java/com/barbanyaga/androiddisplay/ContentPackManagment/DataModel/Xml/Project.java
@@ -0,0 +1,10 @@
+package com.barbanyaga.androiddisplay.ContentPackManagment.DataModel.Xml;
+
+import org.simpleframework.xml.Root;
+
+/**
+ * Created by barbanyaga on 22.03.2015.
+ */
+@Root
+public class Project {
+}
