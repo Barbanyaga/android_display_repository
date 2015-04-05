0a12dc1c4fcd207c0be12dcf79490c18c8239d84
barbanyaga <barbanyaga@gmail.com>
2015-04-05 15:44:56 +0400

    Импорт XML

---
+++ app/src/main/java/com/barbanyaga/androiddisplay/ContentPackManagment/Tasks/Task.java
@@ -0,0 +1,20 @@
+package com.barbanyaga.androiddisplay.ContentPackManagment.Tasks;
+
+import com.barbanyaga.androiddisplay.ContentPackManagment.DataModel.Project;
+
+import org.joda.time.DateTime;
+
+/**
+ * Created by barbanyaga on 05.04.2015.
+ * Задача
+ */
+public class Task {
+    /**
+     * Дата выполнения задачи
+     */
+    DateTime dateShow;
+    /**
+     * Проект для отображения
+     */
+    Project project;
+}
---
+++ app/src/main/java/com/barbanyaga/androiddisplay/ContentPackManagment/Tasks/TaskCreator.java
@@ -0,0 +1,19 @@
+package com.barbanyaga.androiddisplay.ContentPackManagment.Tasks;
+
+import com.barbanyaga.androiddisplay.ContentPackManagment.DataModel.MasterProject;
+
+import java.util.List;
+
+/**
+ * Created by barbanyaga on 05.04.2015.
+ * Создаём на основе мастер проекта список задач для выполнения
+ */
+public class TaskCreator {
+    public TaskCreator() {
+    }
+
+    public List<Task> createTasks(MasterProject masterProject) {
+        List<Task> tasks = null;
+        return tasks;
+    }
+}
